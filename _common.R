bc1a8661fe110e52ea7ca25cbe0148f4efb66dd5
Damurka <>
2025-01-19 14:39:08 +0300

    Updated the documentation

--- _common.R
+++ _common.R
@@ -24,32 +24,3 @@ options(
 )
 
 ggplot2::theme_set(ggplot2::theme_gray(12))
-
-# use results: "asis" when setting a status for a chapter
-status <- function(type) {
-  status <- switch(type,
-                   polishing = "should be readable but is currently undergoing final polishing",
-                   restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
-                   drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
-                   complete = "is largely complete and just needs final proof reading",
-                   stop("Invalid `type`", call. = FALSE)
-  )
-  
-  class <- switch(type,
-                  polishing = "note",
-                  restructuring = "important",
-                  drafting = "important",
-                  complete = "note"
-  )
-  
-  cat(paste0(
-    "\n",
-    ":::: status\n",
-    "::: callout-", class, " \n",
-    "You are reading the work-in-progress second edition of R for Data Science. ",
-    "This chapter ", status, ". ",
-    "You can find the complete first edition at <https://r4ds.had.co.nz>.\n",
-    ":::\n",
-    "::::\n"
-  ))
-}
